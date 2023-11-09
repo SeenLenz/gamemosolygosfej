f0e77b13283f6ccd8c391e48c2169444c1a57a5d
Papp Adam <papp.adam@students.jedlik.eu>
2023-11-09 17:31:40 +0100

    fklasjfkl

--- frontend/src/application/gamelogic/roles/role.ts
+++ frontend/src/application/gamelogic/roles/role.ts
@@ -2,13 +2,16 @@ import { Lobby, NetworkRenderable, WorkerMsg } from "../../../../../types";
 import { camera, network } from "../../../app";
 import { Effect } from "../../base/effects";
 import { GameObject } from "../../base/gameobject";
+import { Player } from "../player";
 
 export interface Role {
     render(delta_time: number): void;
 }
 
 export class PlayerRole implements Role {
-    constructor() {}
+    constructor() {
+        camera.focus_on(new Player([96, 96], [100, -500]));
+    }
 
     render(delta_time: number) {
         GameObject.objects.forEach((go) => {
@@ -22,7 +25,7 @@ export class PlayerRole implements Role {
     }
 }
 
-export class EvilRole implements Role {
+export class Observer implements Role {
     network_renderer: NetworkRenderable[] = [];
     constructor() {}
 

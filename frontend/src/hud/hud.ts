35986a6a528e4514877044e3a2d1c4ddb1c04c55
Zoltai Balázs <zoltai.balazs@students.jedlik.eu>
2023-11-17 09:37:34 +0100

    shop first steps

--- frontend/src/hud/hud.ts
+++ frontend/src/hud/hud.ts
@@ -7,15 +7,17 @@ export class Hud {
     
     constructor() {
         const body = document.body;
-        const main_canvas = document.getElementById('main_canvas');
+        const act_first_canvas = document.querySelector('body canvas:nth-of-type(1)');
+        console.log(act_first_canvas?.id);
         const canvas = <HTMLCanvasElement>document.createElement('canvas');
 
         this.healthBar = new Image();
         this.ctx = canvas.getContext('2d') as CanvasRenderingContext2D;            
         
+        canvas.className = 'ui_canvas';
         canvas.id = 'hud_canvas';
         
-        body?.insertBefore(canvas, main_canvas);
+        body?.insertBefore(canvas, act_first_canvas);
     };
     
     run() {
@@ -24,8 +26,16 @@ export class Hud {
         this.healthBar.onload = () => {
                 this.ctx.imageSmoothingEnabled = false;                
                                 
-                console.log(this.ctx.imageSmoothingEnabled);
                 this.ctx.drawImage(this.healthBar, 0, 5, 54, 18);
             }
+    }
+
+    openShop() {
+        this.ctx.fillStyle = 'blue';
+        this.ctx.fillRect(75, 50, 50, 20);
+    }
+
+    closeShop() {
+
     }
 }

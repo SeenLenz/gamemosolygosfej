e4f704901314d0e54b03c7921859bc478ea87305
[Zoltai Balazs] <balazs.zoltai@gmail.com>
2023-11-22 15:59:31 +0100

    start fade effect & press any

--- frontend/src/ui/start.ts
+++ frontend/src/ui/start.ts
@@ -1,7 +1,7 @@
 import { interpolate } from "../lin_alg";
 import { Hud, clearHud } from "./hud";
 
-let opacity = 0;
+let opacity = 0.0;
 let fadeDirection = 1;
 
 export class Start {
@@ -11,15 +11,19 @@ export class Start {
 
     constructor() {
         const body = document.body;
-        const mainCanvas = document.querySelector('body canvas:nth-of-type(1)');
+        const mainCanvas = document.querySelector('body canvas:nth-of-type(1)') as HTMLCanvasElement;
 
-        this.canvas = document.createElement('canvas') as HTMLCanvasElement;
+        this.canvas = document.createElement('canvas') as HTMLCanvasElement;        
         this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
 
         this.canvas.className = 'ui_canvas';
-        this.canvas.id = 'canvas_start';
-        
-        
+        this.canvas.id = 'start_canvas';
+        this.canvas.setAttribute('tabindex', '0');
+        this.canvas.focus();       
+         
+        window.addEventListener('keydown', (event:KeyboardEvent) => {
+          console.log(event.key);
+        });        
 
         body.insertBefore(this.canvas, mainCanvas);
     }
@@ -32,30 +36,25 @@ export class Start {
         
         const img = new Image();
         img.src = './textures/hud/background.png';
-
-        img.onload = () => {
-            this.animate(img);
-        }
-
-        this.canvas.addEventListener('click', () => {
-           console.log('click');
-        });
+        
+        this.animate(img);
+        
+        this.ctx.globalAlpha = 1.0;
     }
 
     animate(img:HTMLImageElement) {
-        opacity += 0.02 * fadeDirection;
+      opacity += 0.01 * fadeDirection;
 
-          if (opacity > 1) {
-            opacity = 1;
-            fadeDirection = -1;
-          } else if (opacity < 0) {
-            opacity = 0;
-            fadeDirection = 1;
-          }
+      if (opacity > 1) {
+        opacity = 1;
+        fadeDirection = -1;
+      }
+      else if (opacity < 0) {
+        opacity = 0;
+        fadeDirection = 1;
+      }
 
-          this.ctx.globalAlpha = opacity;
-
-          this.ctx.drawImage(img, this.canvas.width*0.5-img.width*0.5, this.canvas.height*0.5-img.height*0.5, img.width, img.height);
-          this.ctx.globalAlpha = 1.0;
-        }    
+      this.ctx.globalAlpha = opacity;
+      this.ctx.drawImage(img, this.canvas.width*0.5-img.width*0.5, this.canvas.height*0.5-img.height*0.5, img.width, img.height);
+    }
 }

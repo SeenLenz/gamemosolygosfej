f091639d3d4864bcb2b9e118ca4b6e59576e2ee8
[Zoltai Balazs] <balazs.zoltai@gmail.com>
2023-11-22 19:01:52 +0100

    press any -> lobby select apears

--- frontend/src/ui/start.ts
+++ frontend/src/ui/start.ts
@@ -1,8 +1,10 @@
+import { event } from "../app";
 import { interpolate } from "../lin_alg";
 import { Hud, clearHud } from "./hud";
 
 let opacity = 0.0;
 let fadeDirection = 1;
+let startKeyPressed = false;
 
 export class Start {
 
@@ -12,37 +14,44 @@ export class Start {
     constructor() {
         const body = document.body;
         const mainCanvas = document.querySelector('body canvas:nth-of-type(1)') as HTMLCanvasElement;
-
+        
         this.canvas = document.createElement('canvas') as HTMLCanvasElement;        
         this.ctx = this.canvas.getContext('2d') as CanvasRenderingContext2D;
-
+        
         this.canvas.className = 'ui_canvas';
         this.canvas.id = 'start_canvas';
         this.canvas.setAttribute('tabindex', '0');
         this.canvas.focus();       
-         
-        window.addEventListener('keydown', (event:KeyboardEvent) => {
+        
+        const kbEvent = (event:KeyboardEvent) => {
+          startKeyPressed = true;
           console.log(event.key);
-        });        
+          this.menuView();
+          window.removeEventListener('keydown', kbEvent);
+
+        }
+
+        window.addEventListener('keydown', kbEvent);
 
         body.insertBefore(this.canvas, mainCanvas);
     }
 
     run() {
         clearHud(this.ctx, this.canvas);
-
+        
         this.ctx.fillStyle = 'blue';
         this.ctx.fillRect(0,0,this.canvas.width, this.canvas.height);
-        
-        const img = new Image();
-        img.src = './textures/hud/background.png';
-        
-        this.animate(img);
+        if (!startKeyPressed) {
+          this.animate();
+        }
         
         this.ctx.globalAlpha = 1.0;
     }
 
-    animate(img:HTMLImageElement) {
+    animate() {
+      const img = new Image();
+      img.src = './textures/hud/background.png';
+
       opacity += 0.01 * fadeDirection;
 
       if (opacity > 1) {
@@ -56,5 +65,11 @@ export class Start {
 
       this.ctx.globalAlpha = opacity;
       this.ctx.drawImage(img, this.canvas.width*0.5-img.width*0.5, this.canvas.height*0.5-img.height*0.5, img.width, img.height);
+    }
+
+    menuView() {
+      const div = document.querySelector(`body div:nth-of-type(1)`);
+      div?.setAttribute('style', 'postition: relative');
+      console.log(div);
     }
 }
